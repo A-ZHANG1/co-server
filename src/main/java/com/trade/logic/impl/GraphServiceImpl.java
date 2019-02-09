7fe038bc54c4d46a176d2072d4f169b255a2d730
wanyue <shiredang@126.com>
2019-02-09 22:52:59 +0800

    refactor: refacor bfs-subgraph-generation with map & stream

--- src/main/java/com/trade/logic/impl/GraphServiceImpl.java
+++ src/main/java/com/trade/logic/impl/GraphServiceImpl.java
@@ -10,9 +10,8 @@ import com.trade.web.response.GeneralResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by Wayne.A.Z on 2019/2/7.
@@ -29,51 +28,63 @@ public class GraphServiceImpl implements GraphService{
     @Autowired
     LinkService linkService;
 
-    List<Company> visited =  new ArrayList<Company>();//已被访问过的元素
-    List<Integer> depthList = new ArrayList<>();//元素深度
 
-    @Override
-    public GeneralResponse<Double> getCompanyWeight(String companyName) {
+    public Map<Company,Integer> getSubGraph(String companyName){
 
-        GeneralResponse<Double> resp=new GeneralResponse<>();
-        double companyWeight = 0;
-
-        //bfs为结点标depth
         LinkedList<Company> bfsQueue = new LinkedList<Company>();//用linkedlist实现队列存放一次要遍历的元素
         LinkedList<Integer> depthQueue = new LinkedList<Integer>();//用linkedlist实现队列存放元素层数
 
-        List<Company> linkedCompanyNodes;
+        Map<Company,Integer> visitedMap = new HashMap<>();
 
         bfsQueue.add(companyMapper.getCompanyByCompanyName(companyName));
         depthQueue.add(0);
 
         while(!bfsQueue.isEmpty()){
-            Company companyNode=bfsQueue.remove();
-            Integer depth = depthQueue.remove();
-            if(!visited.contains(companyNode)){
-                visited.add(companyNode);
-                depthList.add(depth);
-                linkedCompanyNodes = linkService.getLinkedNodesOfC(companyNode);
-                for(Company c : linkedCompanyNodes){
+            Company companyNode = bfsQueue.remove();
+            int depth=depthQueue.remove();
+            visitedMap.put(companyNode,depth);
+            for(Company c : linkService.getLinkedNodesOfC(companyNode)){
+                if(!visitedMap.containsKey(c)){
                     bfsQueue.add(c);
                     depthQueue.add(depth+1);
                 }
             }
         }
+        return visitedMap;
+    }
+
+    @Override
+    public GeneralResponse<Double> getCompanyWeight(String companyName) {
+
+        GeneralResponse<Double> resp=new GeneralResponse<>();
 
 
-//        Double CompanyWeight=new Double(companyWeight);
+        double companyWeight = 0;
+
+
         resp.setObj(companyWeight);
         return resp;
     }
 
     @Override
-    public GeneralResponse<List<Link>> getLinksSurroundingCompany(String companyName) {
-        return null;
+    public GeneralResponse<List<Company>> getNodesSurroundingCompany(String companyName) {
+        GeneralResponse<List<Company>> resp = new GeneralResponse<>();
+        Map<Company,Integer> visitedMap = this.getSubGraph(companyName);
+
+        List<Company> nodesSurroundingCompany;
+
+        nodesSurroundingCompany=visitedMap.entrySet().stream().filter(e -> e.getValue()<3)
+                .map(e->e.getKey()).collect(Collectors.toList());
+
+        resp.setObj(nodesSurroundingCompany);
+        return resp;
     }
 
     @Override
-    public GeneralResponse<List<Company>> getNodesSurroundingCompany(String companyName) {
-        return null;
+    public GeneralResponse<List<Link>> getLinksSurroundingCompany(String companyName) {
+        GeneralResponse<List<Link>> resp = new GeneralResponse<>();
+
+        return resp;
     }
+
 }
